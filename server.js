d5943865f3c5e092a6bdabf56ceba2609c67f076
Dom Steil <dominicsteil@hotmail.com>
2016-06-22 11:23:01 -0700

    adding more detail

--- server.js
+++ server.js
@@ -221,24 +221,7 @@ controller.hears(['Create quote', 'new quote', 'Quote', 'New proposal', 'Create 
                 .then(quote => {
                     bot.reply(message, {
                         text: "I created the quote:",
-                        attachments: [
-            {
-                
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"Configure Products",
-                        "text": "Yes",
-                        "value": "yes",
-                        "style": "Primary",
-                        "type": "button",
-                    }
-                ]
-            }
-        ] 
-
-
+                        attachments: formatter.formatQuote(quote)
                     });
                     convo.next();
                 })
