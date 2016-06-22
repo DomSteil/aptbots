420fe8f122fcef9d39cc3cb31778a13d5255d238
Dom Steil <dominicsteil@hotmail.com>
2016-06-22 09:54:27 -0700

    adding more detail

--- server.js
+++ server.js
@@ -498,52 +498,28 @@ controller.hears(['create ISR', 'new ISR', 'log ISR', ], 'direct_message,direct_
 controller.hears('interactive', 'direct_message', function(bot, message) {
 
     bot.reply(message, {
-    "text": "New comic book alert!",
-    "attachments": [
-        {
-            "title": "The Further Adventures of Slackbot",
-            "fields": [
-                {
-                    "title": "Volume",
-                    "value": "1",
-                    "short": true
-                },
-                {
-                    "title": "Issue",
-                    "value": "3",
-            "short": true
-                }
-            ],
-            "author_name": "Stanford S. Strickland",
-            "author_icon": "https://api.slack.com/img/api/homepage_custom_integrations-2x.png",
-            "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"
-        },
-        {
-            "title": "Synopsis",
-            "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
-        },
-        {
-            "fallback": "Would you recommend it to customers?",
-            "title": "Would you recommend it to customers?",
-            "callback_id": "comic_1234_xyz",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "recommend",
-                    "text": "Recommend",
-                    "type": "button",
-                    "value": "recommend"
-                },
-                {
-                    "name": "no",
-                    "text": "No",
-                    "type": "button",
-                    "value": "bad"
-                }
-            ]
-        }
-    ]
-}
+        attachments:[
+            {
+                title: 'Do you want to interact with my buttons?',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes",
+                        "value": "yes",
+                        "style": "primary",
+                        "type": "button",
+                    },
+                    {
+                        "name":"no",
+                        "text": "No",
+                        "value": "no",
+                        "style": "danger",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
     });    
 });
