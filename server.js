b48ee978d30b4f3a16d19d8532aba7e46e4f9d51
Dom Steil <dominicsteil@hotmail.com>
2016-06-24 15:05:11 -0700

    adding product

--- server.js
+++ server.js
@@ -419,58 +419,6 @@ controller.hears(['New NDA', 'Create NDA', 'NDA'], 'direct_message,direct_mentio
 });
 
 
-controller.hears(['New Product', 'Create Product'], 'direct_message,direct_mention,mention', (bot, message) => {
-
-    let name,
-        code,
-        configType;
-
-    let askProductName = (response, convo) => {
-
-        convo.ask("What is the Product Name?", (response, convo) => {
-            name = response.text;
-            askProductCode(response, convo);
-            convo.next();
-        });
-
-    };
-
-    let askProductCode = (response, convo) => {
-
-        convo.ask("What is the Product Code?", (response, convo) => {
-            code = response.text;
-            askConfigType(response, convo);
-            convo.next();
-        });
-    };
-
-    let askConfigType = (response, convo) => {
-
-        convo.ask("What is the Configuration Type?", (response, convo) => {
-            configType = response.text;
-            salesforce.createProduct({name: name, code: code, configType: configType})
-                .then(product => {
-                    bot.reply(message, {
-                        text: "I created the product",
-                        attachments: formatter.formatProduct(product)
-
-                    });
-                    convo.next();
-                })
-                .catch(error => {
-                    bot.reply(message, error);
-                    convo.next();
-                });
-        });
-
-    };
-
-    bot.reply(message, "OK, I can help you with that!");
-    bot.startConversation(message, askProductName);
-
-});
-
-
 controller.hears(['create ISR', 'new ISR', 'log ISR', ], 'direct_message,direct_mention,mention', (bot, message) => {
 
     let isr,
